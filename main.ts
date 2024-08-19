466f0c0ff5ee559fdac6a3f809829de9fad50224
lumpsoid <mmgeeki@gmail.com>
2024-08-19 15:55:11 +0200

    refactor: command patter for note commands

--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, moment, SearchComponent, Setting } from 'obsidian';
-import { join } from 'path';
+import { App, Modal, Notice, Plugin, PluginSettingTab, SearchComponent, Setting, moment } from 'obsidian';
+import { NoteCommandRegistry } from 'note_commands/command_registry';
+import { NewNoteCommand } from 'note_commands/new_note_command';
 
 // Remember to rename these classes and interfaces!
 
@@ -13,167 +14,55 @@ const DEFAULT_SETTINGS: MyPluginSettings = {
 
 export default class MyPlugin extends Plugin {
 	settings: MyPluginSettings;
+	noteCommands: NoteCommandRegistry;
 
 	async onload() {
 		await this.loadSettings();
 
-		const ribbonApple = this.addRibbonIcon('refresh-ccw', 'exampl reload', (evt: MouseEvent) => {
+		this.addRibbonIcon('refresh-ccw', 'Reload', (evt: MouseEvent) => {
 			const pluginManager = this.app.plugins;
+
 			pluginManager.disablePlugin("sample-plugin");
 			pluginManager.enablePlugin("sample-plugin");
+
 			new Notice("Reloaded");
 		});
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			//new Notice('This is a notice!');
+		this.noteCommands = new NoteCommandRegistry(
+			{
+				vault: this.app.vault,
+				workspace: this.app.workspace,
+				fileManager: this.app.fileManager,
+				moment: moment(),
+			},
+			this.addCommand.bind(this),
+		)
+		this.noteCommands.registerCommand(NewNoteCommand);
 
+		const isSearchPluginEnabled = this.app.internalPlugins.getEnabledPluginById("global-search");
 
-			this.app.workspace.getLeavesOfType('search')[0].view.dom.onChange(()=>{console.log("we are here")});
-			
-			let queue = this.app.workspace.getLeavesOfType('search')[0].view.queue;
-			this.app.workspace.getLeavesOfType('search')[0].view.setQuery('2023');
+		if (isSearchPluginEnabled) {
+			this.addCommand({
+				id: 'open-search-modal',
+				name: 'Search modal window',
+				callback: () => {
+					new SearchModal(this.app).open();
+				}
+			});
 
+			this.addRibbonIcon('search', 'Search plugin', () => {
+				new SearchModal(this.app).open();
+			});
 
-			let itervalID = setInterval(()=>{console.log(this.app.workspace.getLeavesOfType('search')[0].view.queue.queue);}, 500)
+			// this.addSettingTab(new SampleSettingTab(this.app, this));
 
-setTimeout(() => {clearInterval(itervalID);}, 5000)
-
-			let result = this.app.workspace.getLeavesOfType('search')[0].view.dom.resultDomLookup;
-
-			//this.app.fileManager.generateMarkdownLink(cFile, cFile.path)
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		} else {
+			new Notice("Please enable the search core plugin!");
+		}
 
 		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		// const statusBarItemEl = this.addStatusBarItem();
-		// statusBarItemEl.setText('Status Bar Text');
-
-		this.addCommand({
-			id: 'simple-my-editor-1',
-			name: '11',
-			editorCallback: async (editor: Editor) => {
-				const parentFile = this.app.workspace.getActiveFile();
-				if (parentFile === null) return;
-				const parentFileLink = this.app.fileManager.generateMarkdownLink(parentFile, parentFile.path);
-
-				let fileTitleNew; // add as setting
-				let contentNew; // add as setting
-				let tagLineNew = "#tag"; // add as setting
-
-				const cursorFrom = editor.getCursor("from");
-				const cursorTo = editor.getCursor("to");
-				const lineCurrent = editor
-					.getLine(cursorFrom.line)
-					.trimEnd();
-				
-				// TODO add processing of where to put new link
-				// after or berfore text on the line
-				let lineNew;
-
-				// multi line selection
-				if (cursorFrom.line != cursorTo.line) {
-					const selectedText = editor.getSelection().split('\n');
-					editor.replaceRange(
-						'',
-						cursorFrom,
-						cursorTo,
-					);
-					fileTitleNew = selectedText[0];
-					contentNew = selectedText
-						.slice(1)
-						.join('\n');
-					lineNew = `${lineCurrent} $timestamp`;
-
-				// single line selection
-				} else if (cursorFrom.ch != cursorTo.ch) {
-					fileTitleNew = editor.getSelection().trim();
-					contentNew = "- ";
-					lineNew = lineCurrent.replace(fileTitleNew, `${fileTitleNew} $timestamp`)
-
-				// without selection
-				// like if (cursorFrom.ch == cursorTo.ch) 
-				} else {
-					fileTitleNew = lineCurrent.replace(/^\s*(-\s)?/,'');;
-					contentNew = "- ";
-					lineNew = `${lineCurrent} $timestamp`;
-				}
-
-				const cursorMarker = '$|';
-
-				const timestampTemplate = "YYYYMMDDHHmmss"; // add as settings
-				const timestamp = moment().format(timestampTemplate);
-				const fileNameNew = `${timestamp}.md`
-				const fileContent = `# ${fileTitleNew}\n${tagLineNew}${cursorMarker}\n${contentNew}\n- ${parentFileLink}` // read from user's template
-
-				const placeholderOffset = fileContent.indexOf(cursorMarker);
-				const fileContentFinal = fileContent.replace(cursorMarker, '');
-
-				const fileNew = await this.app.vault.create(fileNameNew, fileContentFinal);
-				const fileLinkNew = this.app.fileManager.generateMarkdownLink(fileNew, fileNew.path);
-
-				editor.setLine(
-					cursorFrom.line,
-					lineNew.replace('$timestamp', fileLinkNew),
-				);
-
-				// editor.getLine
-				await this.app.workspace.getLeaf().openFile(fileNew);
-
-				editor.setCursor(editor.offsetToPos(placeholderOffset));
-			},
-		});
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'mysearch',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		// this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		const statusBarItemEl = this.addStatusBarItem();
+		statusBarItemEl.setText('Status Bar Text');
 	}
 
 	onunload() {
@@ -189,34 +78,38 @@ setTimeout(() => {clearInterval(itervalID);}, 5000)
 	}
 }
 
-class SampleModal extends Modal {
+class SearchModal extends Modal {
 	constructor(app: App) {
 		super(app);
 	}
 
 	async onOpen() {
 		const { contentEl } = this;
-		contentEl.setText('Woah!');
-		/* eslint-disable @typescript-eslint/no-explicit-any */
-		const searchPlugin = (
-			this.app as any
-		).internalPlugins.getPluginById("global-search");
-		/* eslint-enable @typescript-eslint/no-explicit-any */
-		const search = searchPlugin && searchPlugin.instance;
 
-		if (searchPlugin && searchPlugin.instance) {
-			console.log(search);
-			// this is working
-			//console.log(this.app.workspace.getLeavesOfType('search')[0].view.dom.resultDomLookup);
-		} else {
-			new Notice("Please enable the search core plugin!");
-		}
+		contentEl.empty();
+
+		new SearchComponent(contentEl)
+			// TODO debounce
+			.onChange(startSearch);
 	}
 
 	onClose() {
 		const { contentEl } = this;
 		contentEl.empty();
 	}
+}
+
+function startSearch(query: string): void {
+	const searchLeaf = this.app.workspace.getLeavesOfType('search')[0].view;
+	console.log(query)
+
+	searchLeaf.setQuery(query);
+
+	const results = searchLeaf.dom.resultDomLookup;
+
+	console.log(results);
+	// this.app.fileManager.generateMarkdownLink(cFile, cFile.path)
+
 }
 
 class SampleSettingTab extends PluginSettingTab {
---
+++ note_commands/new_note_command.ts
@@ -0,0 +1,89 @@
+import { Editor, Command, } from "obsidian";
+import { BaseNoteCommand } from "./base_note_command";
+import { Env } from "./env";
+
+export class NewNoteCommand extends BaseNoteCommand {
+	constructor() {super()}
+
+	getCommandId(): string {
+		return 'note-creator-command';
+	}
+	getCommandName(): string {
+		return '11';
+	}
+	// Command
+	create(env: Env): Command {
+		return {
+			id: this.getCommandId(),
+			name: this.getCommandName(),
+			editorCallback: async (editor: Editor) => {
+				const parentFile = env.workspace.getActiveFile();
+				if (parentFile === null) return;
+				const parentFileLink = env.fileManager.generateMarkdownLink(parentFile, parentFile.path);
+
+				let fileTitleNew; // add as setting
+				let contentNew = '- '; // add as setting
+				let tagLineNew = "#tag"; // add as setting
+
+				const cursorFrom = editor.getCursor("from");
+				const cursorTo = editor.getCursor("to");
+				const lineCurrent = editor
+					.getLine(cursorFrom.line)
+					.trimEnd();
+
+				// TODO add processing of where to put new link
+				// after or berfore text on the line
+				let lineNew;
+
+				// multi line selection
+				if (cursorFrom.line != cursorTo.line) {
+					const selectedText = editor.getSelection().split('\n');
+					editor.replaceRange(
+						'',
+						cursorFrom,
+						cursorTo,
+					);
+					fileTitleNew = selectedText[0];
+					contentNew = selectedText
+						.slice(1)
+						.join('\n');
+					lineNew = `${lineCurrent} $timestamp`;
+
+					// single line selection
+				} else if (cursorFrom.ch != cursorTo.ch) {
+					fileTitleNew = editor.getSelection().trim();
+					lineNew = lineCurrent.replace(fileTitleNew, `${fileTitleNew} $timestamp`)
+
+					// without selection
+					// like if (cursorFrom.ch == cursorTo.ch) 
+				} else {
+					fileTitleNew = lineCurrent.replace(/^\s*(-\s)?/, '');
+					lineNew = `${lineCurrent} $timestamp`;
+				}
+
+				const cursorMarker = '$|';
+
+				const timestampTemplate = "YYYYMMDDHHmmss"; // add as settings
+				const timestamp = env.moment.format(timestampTemplate);
+				const fileNameNew = `${timestamp}.md`
+				const fileContent = `# ${fileTitleNew}\n${tagLineNew}${cursorMarker}\n${contentNew}\n- ${parentFileLink}` // read from user's template
+
+				const placeholderOffset = fileContent.indexOf(cursorMarker);
+				const fileContentFinal = fileContent.replace(cursorMarker, '');
+
+				const fileNew = await env.vault.create(fileNameNew, fileContentFinal);
+				const fileLinkNew = env.fileManager.generateMarkdownLink(fileNew, fileNew.path);
+
+				editor.setLine(
+					cursorFrom.line,
+					lineNew.replace('$timestamp', fileLinkNew),
+				);
+
+				// editor.getLine
+				await env.workspace.getLeaf().openFile(fileNew);
+
+				editor.setCursor(editor.offsetToPos(placeholderOffset));
+			},
+		};
+	};
+}
