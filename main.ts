27181c609244230f81df1261cd381115a896786a
lumpsoid <mmgeeki@gmail.com>
2024-08-19 03:48:16 +0200

    feat: added note insert feature

--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, SearchComponent, Setting } from 'obsidian';
+import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, moment, SearchComponent, Setting } from 'obsidian';
+import { join } from 'path';
 
 // Remember to rename these classes and interfaces!
 
@@ -16,40 +17,112 @@ export default class MyPlugin extends Plugin {
 	async onload() {
 		await this.loadSettings();
 
+		const ribbonApple = this.addRibbonIcon('refresh-ccw', 'exampl reload', (evt: MouseEvent) => {
+			const pluginManager = this.app.plugins;
+			pluginManager.disablePlugin("sample-plugin");
+			pluginManager.enablePlugin("sample-plugin");
+			new Notice("Reloaded");
+		});
+
 		// This creates an icon in the left ribbon.
 		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
 			// Called when the user clicks the icon.
 			//new Notice('This is a notice!');
-			//console.log(searchPlugin.views.search);
 
-			console.log('search leaf');
-			this.app.workspace.getLeavesOfType('search')[0].view.setQuery('language');
 
-			if (searchPlugin && searchPlugin.instance) {
-				// this is working
-				console.log(this.app.workspace.getLeavesOfType('search')[0].view.dom.resultDomLookup);
-				this.app.fileManager.generateMarkdownLink(cFile, cFile.path)
-			} else {
-				new Notice("Please enable the search core plugin!");
-			}
+			this.app.workspace.getLeavesOfType('search')[0].view.dom.onChange(()=>{console.log("we are here")});
+			
+			let queue = this.app.workspace.getLeavesOfType('search')[0].view.queue;
+			this.app.workspace.getLeavesOfType('search')[0].view.setQuery('2023');
+
+
+			let itervalID = setInterval(()=>{console.log(this.app.workspace.getLeavesOfType('search')[0].view.queue.queue);}, 500)
+
+setTimeout(() => {clearInterval(itervalID);}, 5000)
+
+			let result = this.app.workspace.getLeavesOfType('search')[0].view.dom.resultDomLookup;
+
+			//this.app.fileManager.generateMarkdownLink(cFile, cFile.path)
 		});
 		// Perform additional things with the ribbon
 		ribbonIconEl.addClass('my-plugin-ribbon-class');
 
 		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+		// const statusBarItemEl = this.addStatusBarItem();
+		// statusBarItemEl.setText('Status Bar Text');
 
 		this.addCommand({
 			id: 'simple-my-editor-1',
-			name: '1create new note',
-			editorCallback: (editor: Editor) => {
-				editor.replaceRange(
-					moment().format("YYYY-MM-DD"),
-					editor.getCursor()
+			name: '11',
+			editorCallback: async (editor: Editor) => {
+				const parentFile = this.app.workspace.getActiveFile();
+				if (parentFile === null) return;
+				const parentFileLink = this.app.fileManager.generateMarkdownLink(parentFile, parentFile.path);
+
+				let fileTitleNew; // add as setting
+				let contentNew; // add as setting
+				let tagLineNew = "#tag"; // add as setting
+
+				const cursorFrom = editor.getCursor("from");
+				const cursorTo = editor.getCursor("to");
+				const lineCurrent = editor
+					.getLine(cursorFrom.line)
+					.trimEnd();
+				
+				// TODO add processing of where to put new link
+				// after or berfore text on the line
+				let lineNew;
+
+				// multi line selection
+				if (cursorFrom.line != cursorTo.line) {
+					const selectedText = editor.getSelection().split('\n');
+					editor.replaceRange(
+						'',
+						cursorFrom,
+						cursorTo,
+					);
+					fileTitleNew = selectedText[0];
+					contentNew = selectedText
+						.slice(1)
+						.join('\n');
+					lineNew = `${lineCurrent} $timestamp`;
+
+				// single line selection
+				} else if (cursorFrom.ch != cursorTo.ch) {
+					fileTitleNew = editor.getSelection().trim();
+					contentNew = "- ";
+					lineNew = lineCurrent.replace(fileTitleNew, `${fileTitleNew} $timestamp`)
+
+				// without selection
+				// like if (cursorFrom.ch == cursorTo.ch) 
+				} else {
+					fileTitleNew = lineCurrent.replace(/^\s*(-\s)?/,'');;
+					contentNew = "- ";
+					lineNew = `${lineCurrent} $timestamp`;
+				}
+
+				const cursorMarker = '$|';
+
+				const timestampTemplate = "YYYYMMDDHHmmss"; // add as settings
+				const timestamp = moment().format(timestampTemplate);
+				const fileNameNew = `${timestamp}.md`
+				const fileContent = `# ${fileTitleNew}\n${tagLineNew}${cursorMarker}\n${contentNew}\n- ${parentFileLink}` // read from user's template
+
+				const placeholderOffset = fileContent.indexOf(cursorMarker);
+				const fileContentFinal = fileContent.replace(cursorMarker, '');
+
+				const fileNew = await this.app.vault.create(fileNameNew, fileContentFinal);
+				const fileLinkNew = this.app.fileManager.generateMarkdownLink(fileNew, fileNew.path);
+
+				editor.setLine(
+					cursorFrom.line,
+					lineNew.replace('$timestamp', fileLinkNew),
 				);
 
-				editor.getLine
+				// editor.getLine
+				await this.app.workspace.getLeaf().openFile(fileNew);
+
+				editor.setCursor(editor.offsetToPos(placeholderOffset));
 			},
 		});
 
@@ -100,7 +173,7 @@ export default class MyPlugin extends Plugin {
 		});
 
 		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		// this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {
