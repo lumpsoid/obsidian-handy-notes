9cb6e978fead357a071e2ca622bf179b77c15312
lumpsoid <mmgeeki@gmail.com>
2024-08-20 16:09:05 +0200

    refactor: base note action action method added

--- src/note_commands/base_note_action.ts
+++ src/note_commands/base_note_action.ts
@@ -8,6 +8,7 @@ export abstract class BaseNoteAction {
 	public abstract getCommandId(): string;
 	public abstract getCommandName(): string;
 	public abstract command(env: Env): Command;
+	public abstract action(env: Env, editor: Editor): Promise<void> ;
 
     protected getCursorMarkers(editor: Editor) {
         const cursorFrom = editor.getCursor("from");
