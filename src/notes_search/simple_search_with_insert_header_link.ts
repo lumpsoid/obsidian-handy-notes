e87dde71581aa2fce2b3c0fab0392b6b26d1531a
lumpsoid <mmgeeki@gmail.com>
2024-08-20 15:52:07 +0200

    feat: added search with insert link and header

---
+++ src/notes_search/simple_search_with_insert_header_link.ts
@@ -0,0 +1,53 @@
+import { App, Command, Editor, Notice } from "obsidian";
+import { BaseSearch } from "./base_search";
+import { getAllNotesWithContent, getEditor, insertLinkOnCursorSafe } from "../utils";
+import { SimpleNotesSearch } from "./modals/simple_notes_search";
+
+export class SimpleSearchWithInsertHeaderLink extends BaseSearch {
+	static COMMAND_ID: string = 'simple-search-with-insert-header-link';
+
+	public getSearchId(): string {
+		return SimpleSearchWithInsertHeaderLink.COMMAND_ID;
+	}
+	public getSearchName(): string {
+		return 'Simple search with insert link and header'
+	}
+	public command(app: App): Command {
+		return {
+			id: this.getSearchId(),
+			name: this.getSearchName(),
+			editorCallback: () => {
+				this.action(app);
+			},
+		};
+	};
+	public ribbonIcon(app: App): [string, string, (evt: MouseEvent) => any] {
+		const icon = 'external-link';
+		const title = this.getSearchName();
+		const callback = () => {
+			this.action(app);
+			return;
+		};
+		return [icon, title, callback];
+	}
+	public async action(app: App, editor?: Editor | undefined): Promise<void> {
+		if (editor === undefined) {
+			editor = getEditor(app);
+			if (editor === undefined) {
+				new Notice('No editor available');
+				return;
+			}
+		}
+		new SimpleNotesSearch(
+			app,
+			getAllNotesWithContent(app),
+			"Search notes to insert a link...",
+			undefined,
+			(file, noteContent) => {
+				const markdownLink = app.fileManager.generateMarkdownLink(file, file.path);
+				const header = noteContent.split('\n')[0].replace('#', '').trim();
+				insertLinkOnCursorSafe(editor!, `${markdownLink} ${header}`);
+			}
+		).open();
+	}
+}
